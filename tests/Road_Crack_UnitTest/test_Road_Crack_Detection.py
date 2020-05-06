3cf8682dc9c4827813fbc496e68de398650b859e
Debkanya1998 <63354603+Debkanya1998@users.noreply.github.com>
2020-05-06 11:32:55 +0530

    Updated unit test for road crack detection

--- tests/Road_Crack_UnitTest/test_Road_Crack_Detection.py
+++ tests/Road_Crack_UnitTest/test_Road_Crack_Detection.py
@@ -1,37 +1,42 @@
 
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator, load_img
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense
-from tensorflow.python.keras import backend as K
-from tensorflow.python.keras import layers
-from tensorflow.python.keras import models
-import keras
-from tensorflow.python.keras import optimizers
-
-
-from tensorflow.python.keras.models import load_model
-from tensorflow.python.keras.preprocessing import image
+from keras.models import Sequential
+sys.path.append('../src/')
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.applications import VGG16
+from keras.applications import VGG16
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+import os
+import cv2
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+from keras.models import load_model
+from keras.preprocessing import image
+
 
 from Road_Crack_Detection import  PictureManager 
 
 import unittest
 
 class test_Picture_Manager(unittest.TestCase):
-    def test_clusterImage_testingcrack(self):
+    def test_cluster_image_testingcrack(self):
         c = PictureManager()
         
-        self.assertEqual(c.test_clusterImage('/content/drive/My Drive/Road/test_set/00003.jpg'),
+        self.assertEqual(c.cluster_image_testing("crack.jpg"),
                         True)
-    def test_clusterImage_testing(self):
+        
+    def test_cluster_image_testinguncrack(self):
         c = PictureManager()
         
-        self.assertEqual(c.test_clusterImage('/content/drive/My Drive/Road/test_set/00002.jpg'),
-                        True)   
-              
+        self.assertEqual(c.cluster_image_testing("non_crack.jpg"),
+                        False)
+        
+        
     
 if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+	unittest.main()
